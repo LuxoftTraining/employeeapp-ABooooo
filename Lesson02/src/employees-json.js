04bb4d2c9dd35141d032c34e1ca8db535a0b5db8
ABooooo <ales.bombek@triera.net>
2023-01-23 10:45:19 +0100

    JS Course

---
+++ Lesson02/src/employees-json.js
@@ -0,0 +1,28 @@
+const DATA = { 
+ employees: [ 
+   { 
+    id: 1, 
+    name: "Peter", 
+    surname: "Peterson", 
+    department: "IT" 
+   },
+   { 
+    id: 2, 
+    name: "Seppi", 
+    surname: "Peterson", 
+    department: "IT" 
+   },
+   { 
+    id: 3, 
+    name: "Josef", 
+    surname: "Peterson", 
+    department: "IT" 
+   },
+   { 
+    id: 4, 
+    name: "Franz", 
+    surname: "Peterson", 
+    department: "IT" 
+   }
+ ] 
+} 
---
+++ Lesson02/src/service.js
@@ -0,0 +1,186 @@
+function findByName(name, surname) {
+    let result = []; 
+    for (var employee of DATA.employees) { 
+        if ((!name || employee.name === name) && (!surname || employee.surname === surname)) { 
+            result.push(employee); 
+        } 
+    } 
+    return result;
+};
+
+function addEmployee(name, surname) { 
+    if (!name || name.length == 0 || !surname || surname.length == 0) { 
+     throw new Error("name and surname should be not empty"); 
+    } 
+    let max = 0;
+    for (let employee of DATA.employees) { 
+     if (employee.id > max) max = employee.id; 
+    } 
+    let id = max + 1; 
+    DATA.employees.push({id, name, surname}); 
+    return id; 
+};
+
+function removeEmployee(id) { 
+    let index = 0; 
+    for (let employee of DATA.employees) { 
+     if (employee.id === id) break; 
+     index++; 
+    } 
+    DATA.employees.splice(index, 1); 
+};
+
+function showEmployee(employee) { 
+    const keys = Object.keys(employee); 
+    console.log("Employee info " + employee["name"] + ":"); 
+    for (let key of keys) { 
+        console.log(key + " = " + employee[key]);
+    } 
+};
+
+function showEmployees() { 
+    // alternative option => DATA.employees.forEach(showEmployee);  
+    for (let employee of DATA.employees) { 
+     showEmployee(employee); 
+    } 
+};
+
+function findById(id) { 
+    for (var employee of DATA.employees) { 
+     if (id == employee.id) { 
+      return employee; 
+     } 
+    } 
+   throw Error("employee with id="+id+"not found!"); 
+};
+
+const employeeMap = {}; 
+function findByIdMap(id) { 
+ if (employeeMap[id]) { 
+  return employeeMap[id]; 
+ } 
+ for (var employee of DATA.employees) { 
+  if (id == employee.id) { 
+   employeeMap[id] = employee; 
+   return employee;
+  } 
+ } 
+};
+
+function addPhone(id, phone) { 
+    const employee = findById(id); 
+    const phones = employee.phones; 
+    if (!phones) { 
+     employee.phones = []; 
+    } 
+    employee.phones.push(phone); 
+};
+
+// setDateOfBirth(1, new Date('May 30, 1980'))
+function setDateOfBirth(id, date) { 
+ const employee = findById(id); 
+ employee.dateOfBirth = date; 
+};
+ 
+function getAge(id) { 
+    const employee = findById(id); 
+    let ageDiff = Date.now() - employee.dateOfBirth.getTime(); 
+    let ageDate = new Date(ageDiff); // miliseconds from epoch 
+    return Math.abs(ageDate.getUTCFullYear() - 1970); 
+};
+
+function formatDate(date) { 
+    let day = date.getDate(); 
+    if (day<10) day = '0'+day; 
+    let month = date.getMonth()+1; 
+    if (month<10) month = '0'+month; 
+    let year = date.getFullYear(); 
+    
+    return day + '.' + month + '.' + year; 
+};
+
+function getEmployeeInfo(id) { 
+    const employee = findById(id); 
+    
+    const phones = employee.phones ? `List of phones: ${employee.phones}` : ''; 
+    const age = employee.dateOfBirth ? `Age: ${getAge(employee.id)}` : ''; 
+
+    return `
+     Name: ${employee.name} 
+     Surname: ${employee.surname} 
+     Date of birth: ${formatDate(employee.dateOfBirth)} 
+     ${phones}  
+     ${age} 
+    `; 
+};
+
+function testEmployee() { 
+    addPhone(133, "555-55-55"); 
+    addPhone(133, "666-66-66"); 
+    setDateOfBirth(133, new Date(2000,1,1)) 
+    const info = getEmployeeInfo(133); 
+    console.log(info); 
+};
+
+function getEmployeeJSON(id) { 
+    const employee = findById(id); 
+    return JSON.stringify(employee); 
+};
+
+function removeEmployee(id) { 
+    let index = 0; 
+    for (let e of DATA.employees) { 
+     if (e.id === id) break; 
+     index++; 
+    } 
+    DATA.employees.splice(index, 1); 
+}
+
+function removeEmployeeUI(id) { 
+    removeEmployee(id); 
+    showEmployees(DATA.employees); 
+}
+
+function setEmployeeManager(id, managerId) {
+    var myArray = DATA.employees;
+    var index = myArray.findIndex((obj => obj.id == id));
+    if (index > -1) {
+        DATA.employees[index].managerRef = managerId; 
+    }    
+};
+
+ function searchEmployees(name, surname, managerRef) { 
+    let results = []; 
+    for (let e of DATA.employees) { 
+     if ((!name || e.name==name) && 
+      (!surname || e.surname==surname) && 
+      (!managerRef || e.managerRef==managerRef)) { 
+      results.push(e); 
+     } 
+    } 
+    return results; 
+};
+
+function saveEmployee() {
+    var id = document.getElementById('idEdit').value;
+    var name = document.getElementById('nameEdit').value;
+    var surname = document.getElementById('surnameEdit').value;
+    
+    var myArray = DATA.employees;
+    var index = myArray.findIndex((obj => obj.id == id));
+    if (index > -1) {
+        DATA.employees[index].name = name;
+        DATA.employees[index].surname = surname; 
+    }    
+
+    document.getElementById('employeesEditPlaceholder').innerHTML = "";
+    showEmployeesEdit(DATA.employees);
+}
+
+
+
+
+
+
+
+    
